b02ad173e229a436637f8949449ed5e7793bc398
Riley <rmcaldwell2015@gmail.com>
2026-08-19 19:17:42 -0400

    Prettier updates and client updates. close to done!

--- src/data/site.ts
+++ src/data/site.ts
@@ -1,11 +1,14 @@
 export const contactInfo = {
-  phone: "(419) 654-8225",
-  phoneHref: "tel:+14196548225",
+  phone: "(419) 740-5531",
+  phoneHref: "tel:+14197405531",
   email: "contact@beaconsearchgroup.com",
-  addressLines: [
-    "Beacon Search Group",
-    "1200 Harbor Point Drive, Suite 240",
-    "Cleveland, OH 44113"
-  ],
+  addressLines: ["100 S. Main Street, 2nd Floor", "Bowling Green, OH 43402"],
   hours: "Monday – Friday, 8:00am – 6:00pm ET"
+};
+
+export const socialLinks = {
+  linkedin: "https://www.linkedin.com/company/beacon-search-group/",
+  facebook: "https://www.facebook.com/profile.php?id=61588100890292",
+  aaronLinkedIn: "https://www.linkedin.com/in/aaron-laird-csp-35a6551b/",
+  alexLinkedIn: "https://www.linkedin.com/in/alexgundy/"
 };
